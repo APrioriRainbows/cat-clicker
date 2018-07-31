279aeb63ad323f22f0a07bd6d7f51252102750b1
Maria Caruso <mariakcaruso@gmail.com>
2018-07-31 04:26:16 +0000

    change simple click function over to classes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 let cat = document.getElementById('cat');
-const counter = document.getElementById('click-number');
-let count = 0;
+let catBox = document.querySelector('body');
+//let count = 0;
+let cats = [];
 
-cat.onclick = function(){
-    counter.innerText ++;  
-};
+class Cat {
+    constructor(name, pic){
+	this.name = name;
+	this.pic = pic;
+	this.count = 0;
+	this.div = document.createElement('div');
+	catBox.appendChild(this.div);
+	this.div.innerHTML = `<img src='${this.pic}' alt='a cat' class='button'><section class="counter">Clicks: <span id="click-number">${this.count}</span></section>`;
+	var self = this;
+	this.div.addEventListener('click', function  click() {
+	    let counter = this.querySelector('#click-number');
+	    self.count ++;
+	    counter.innerText = self.count;
+	})
+    }
+}
+
+let Sheila = new Cat('Sheila','img/cat.jpg');
+let Sasha = new Cat('Sasha', 'img/cat.jpg');
+cats.push(Sheila);
+cats.push(Sasha);
+
+
