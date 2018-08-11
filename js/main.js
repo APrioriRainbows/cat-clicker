65d060dded8b58ef838f6f9758f63ebc358d9e43
Maria Caruso <mariakcaruso@gmail.com>
2018-08-11 18:07:09 +0000

    cat clicker premium specs, add more cats, creating click and load functionality, update css to flex

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let cat = document.getElementById('cat');
-let catBox = document.querySelector('body');
-//let count = 0;
+let catBox = document.getElementById('cat-box');
+let activeCatArea = document.getElementById('clicked-cat');
 let cats = [];
 
 class Cat {
@@ -8,21 +8,34 @@ class Cat {
 	this.name = name;
 	this.pic = pic;
 	this.count = 0;
-	this.div = document.createElement('div');
-	catBox.appendChild(this.div);
-	this.div.innerHTML = `<img src='${this.pic}' alt='a cat' class='button'><section class="counter">Clicks: <span id="click-number">${this.count}</span></section>`;
-	var self = this;
-	this.div.addEventListener('click', function  click() {
-	    let counter = this.querySelector('#click-number');
-	    self.count ++;
-	    counter.innerText = self.count;
+	this.li = document.createElement('li');
+	catBox.appendChild(this.li);
+	this.li.innerHTML = `${this.name}`;
+	var cat = this;
+	this.li.addEventListener('click', function loadCat(){
+	    console.log(cat);
+	    cat.div = document.createElement('div');
+	    activeCatArea.innerHTML = '';
+	    activeCatArea.appendChild(cat.div);
+	    cat.div.innerHTML =`<img src='${cat.pic}' alt='a cat' class='button'><section class="counter">Clicks: <span id="click-number">${cat.count}</span></section><br><section>Kitty Name: ${cat.name}</section>`;
+	    cat.div.addEventListener('click', function  click() {
+		let counter = this.querySelector('#click-number');
+		cat.count ++;
+		counter.innerText = cat.count;
+	    })
 	})
+				 
     }
 }
 
 let Sheila = new Cat('Sheila','img/cat.jpg');
-let Sasha = new Cat('Sasha', 'img/cat.jpg');
+let Sasha = new Cat('Sasha','img/cat2.jpg');
+let Cooper = new Cat('Cooper','img/cat3.jpg');
+let Cordelia = new Cat('Cordelia', 'img/cat4.jpg');
+let KittyCat = new Cat('Kitty Cat AKA Cat', 'img/cat5.gif');
 cats.push(Sheila);
 cats.push(Sasha);
-
+cats.push(Cooper);
+cats.push(Cordelia);
+cats.push(KittyCat);
 
